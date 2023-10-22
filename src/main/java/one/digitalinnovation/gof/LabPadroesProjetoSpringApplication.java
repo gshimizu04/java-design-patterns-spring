72268bdbb3966451b90ea8e97dc931ff77a5557a
Guilherme Comachio Shimizu <gshimizu04@gmail.com>
2023-10-22 14:58:31 -0300

    Added project to remote repository

---
+++ src/main/java/one/digitalinnovation/gof/LabPadroesProjetoSpringApplication.java
@@ -0,0 +1,27 @@
+package one.digitalinnovation.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Projeto Spring Boot gerado via Spring Initializr
+ * Dependências do projeto:
+ * Spring Data JPA
+ * Spring Web
+ * H2 Database
+ * OpenFeign
+ *
+ * @author guics
+ */
+
+@EnableFeignClients
+@SpringBootApplication
+public class LabPadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesProjetoSpringApplication.class, args);
+	}
+
+}
